a50e1ec3627ca62490a0ccdaee357757da53d3eb
Tim Lunn <tim@feathertop.org>
2023-11-25 13:55:04 +1100

    Add options flow to reconfigure integration

--- sonoff_ihost/const.py
+++ sonoff_ihost/const.py
@@ -3,22 +3,24 @@ DOMAIN = "sonoff_ihost"
 PLATFORMS: list[str] = ["light","switch"]
 
 BUTTON_LIST = ['power','pairing', 'security', 'music', 'reset']
+BUTTON1_MODES = [
+    {"value": "0", "label": "ZHA"},
+    {"value": "1", "label": "Zigbee2MQTT"},
+    {"value": "2", "label": "User"},
+]
 
 CONF_BRIGHTNESS = 'brightness'
 CONF_COLOR = 'color'
 CONF_PAIRING = "pairing"
 CONF_STATE = 'state'
 
-DEFAULT_BRIGHTNESS = 100
-DEFAULT_COLOR = [0, 0, 255]
-DEFAULT_PAIRING = "0"
-DEFAULT_STATE = True
+DEFAULTS = {
+    CONF_BRIGHTNESS: 100,
+    CONF_COLOR: [0, 0, 255],
+    CONF_PAIRING: "0",
+    CONF_STATE: True
+}
 
-BUTTON1_MODES = [
-    {"value": "0", "label": "ZHA"},
-    {"value": "1", "label": "Zigbee2MQTT"},
-    {"value": "2", "label": "User"},            
-]
 PAIRING_MODES = ["ZHA", "Zigbee2MQTT", "User"]
 EVENT_SPECIAL_BUTTON = "sonoff_ihost_button"
 
